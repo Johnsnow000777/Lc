6bbd7c7d9ec5bcff68d699e3342d636c134d2faa
Johnsnow000777 <148466323+Johnsnow000777@users.noreply.github.com>
2024-07-22 13:26:13 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "24906331"
+api_hash = "866e8e4637fb269388b50202fb0f169c"
+bot_token = "6921841186:AAFfRHF8EeozBBJhRuP96HzKKkCZk_6Fqqk"
